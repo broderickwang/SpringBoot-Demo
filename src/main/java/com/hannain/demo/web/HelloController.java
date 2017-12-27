2fe1a060c181e133bb607404197c2cd0109a4209
LanCoder <broderick_wang@163.com>
2017-12-27 15:34:48 +0800

    添加web security

--- src/main/java/com/hannain/demo/web/HelloController.java
+++ src/main/java/com/hannain/demo/web/HelloController.java
@@ -1,16 +1,28 @@
 package com.hannain.demo.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by 王成达 on 2017/12/26.
  */
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String index(){
-        return "hello world";
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
     }
 }
